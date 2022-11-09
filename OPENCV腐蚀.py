e7fc30cb4599e7496ee8c14a553e6be650fbad10
zdyzc <616662878@qq.com>
2022-11-09 19:45:55 +0800

    nice！gogogo！

--- OPENCV腐蚀.py
+++ OPENCV腐蚀.py
@@ -4,7 +4,7 @@ import numpy as np
 img=cv2.imread("D:\cv2\chess.png")#"D:\cv2\hujiao.jpg"D:\cv2\chess.png
 #"D:\cv2\fushipengzhang1.jpg"
 kernel=np.ones((3,3),np.uint8)
-dst=cv2.erode(img,kernel,iterations=1)
+dst=cv2.erode(img,kernel,iterations=6)#iterations是腐蚀次数，主要是让白色变少成为黑色
 
 cv2.imshow('img',img)
 cv2.imshow('dst',dst)
